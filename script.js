8248c8b79f397c90e74891eec82d393cdda7735c
Gabriel Shiota <gabrielnogueirashiota@gmail.com>
2023-02-11 20:27:26 -0300

    add image From project

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const button360 = document.getElementById("button3d");
 const imageProduct = document.getElementById("imageProduct");
 
 button360.onclick = () => {
-  imageProduct.src.toggle("sofaGIF.gif");
+  imageProduct.src = "sofaGIF.gif";
 };
