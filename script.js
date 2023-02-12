cfcc438c6683d9f146e6ba99c5241f2a670b3ecf
Gabriel Shiota <gabrielnogueirashiota@gmail.com>
2023-02-11 21:20:24 -0300

    repair in button for toggle image to gif

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ const button360 = document.getElementById("button3d");
 const imageProduct = document.getElementById("imageProduct");
 
 button360.onclick = () => {
-  imageProduct.src = "sofaGIF.gif";
+  if (
+    imageProduct.src ===
+    "file:///C:/Users/gabri/Desktop/JavaScript/BoraCodar/Desafio%232/sofa.png"
+  ) {
+    imageProduct.src = "sofaGIF.gif";
+  } else {
+    imageProduct.src = "sofa.png";
+  }
 };
